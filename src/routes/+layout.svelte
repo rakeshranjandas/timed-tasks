7cc0c6380d0e31ae1d0c3bfd5ea88f167c79b9a1
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2025-07-29 00:43:27 +0530

    Demo - simple, without worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,12 @@
 <script lang="ts">
     import { AppState, setAppState } from '$lib/state/app-state.svelte';
-    import { workerUrl } from '$lib/workers/tick.worker';
     import { input } from '$test/dsa';
     import '../app.css';
-    import { onDestroy, onMount } from 'svelte';
 
-    
-
-    let worker = $state<Worker | null>(null);
-    let appState = $derived.by(() => {
-        if (!worker) return null;
-        return setAppState(input, worker);
-    });
-
-    onMount(() => {
-        worker = new Worker(workerUrl, {
-            type: 'module'
-        });
-    });
-
-    onDestroy(() => {
-        worker?.terminate();
-    });
+    setAppState(input);
 
     let { children } = $props();
 
 </script>
 
-{#if appState}
 {@render children()}
-{/if}
