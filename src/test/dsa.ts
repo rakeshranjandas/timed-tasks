58b45c578ea3780a217c44070e76b00aa7c5d8a2
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2025-07-29 00:48:22 +0530

    Testing - worker

--- src/test/dsa.ts
+++ src/test/dsa.ts
@@ -3,7 +3,7 @@ import type { Phase } from "$lib/types/task.types";
 export const input: Phase[] = [
     {
         phase_name: "Planning",
-        phase_time_in_minutes: 10,
+        phase_time_in_minutes: 5,
         phase_tasks: [
             {
                 task_name: "Draft a solution",
