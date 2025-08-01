2b6ecd5261f23223befa48d6143a80ef0484c1ef
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2025-08-02 00:49:16 +0530

    Set default phases to blank

--- src/lib/input/default_phase.ts
+++ src/lib/input/default_phase.ts
@@ -1,11 +1,7 @@
 import type { Phase } from "$lib/types/task.types";
 
 export const defaultPhase: Phase = {
-    phase_name: "Dummy Phase",
-    phase_time_in_minutes: 10,
-    phase_tasks: [
-        { task_name: "task 1" },
-        { task_name: "task 2" },
-        { task_name: "task 3" },
-    ],
+    phase_name: "",
+    phase_time_in_minutes: 0,
+    phase_tasks: [],
 };
